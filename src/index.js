112bdb5395ca44e16dd0f97d0ee82e63b424af39
Sayuk <sayukbohdan@gmail.com>
2023-03-04 19:40:38 +0200

    o

--- src/index.js
+++ src/index.js
@@ -12,16 +12,16 @@ const btn = document.querySelector('.btn');
 
 
 async function pixabay(){
-    const response = await axios.get('https://pixabay.com/api/');
+    const response = await axios.get(`https://pixabay.com/api/?key=34023502-430bec32b806d37b8bfdc9ad2&q&image_typ=photo&orientation=horizontal&safesearch=true`);
     console.log(response)
 
     //     const response = await fetch(`https://pixabay.com/api/?key=34023502-430bec32b806d37b8bfdc9ad2&q&image_typ=photo&orientation=horizontal&safesearch=true`);
-    //     if (!response.ok) {
-    //         throw new Error(response.statusText)
-    //     }
-    //     const data = await response.json()
-    //     console.log(data);
-    // return data;
+        if (!response.ok) {
+            throw new Error(response.statusText)
+        }
+        const data = await response.json()
+        console.log(data);
+    return data;
 }
 pixabay()
     .then(response => console.log(response))
