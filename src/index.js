d8770819c76eedb1f581597d63ca29ca2972b8b2
Sayuk <sayukbohdan@gmail.com>
2023-03-06 20:25:10 +0200

    S

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
 import './css/styles.css';
 import axios from 'axios';
-// import Notiflix from 'notiflix';
+import card from '../src/templates/card.hbs'
+import Notiflix from 'notiflix';
 // https://pixabay.com/api/
 
 const DEBOUNCE_DELAY = 300;
 const countryForm = document.querySelector('#search-form');
 const btn = document.querySelector('.btn');
+const gallery = document.querySelector('.gallery');
+async function pixabay() {
+    // const response = await axios.get(`https://pixabay.com/api/?key=34023502-430bec32b806d37b8bfdc9ad2&q&image_typ=photo&orientation=horizontal&safesearch=true`);
 
+    // const response = await fetch(`https://pixabay.com/api/?key=34023502-430bec32b806d37b8bfdc9ad2&q&image_typ=photo&orientation=horizontal&safesearch=true`);
+    //         if (!response.ok) {
+    //             throw new Error(response.statusText)
+    //         }
+    //         const data = await response.json()
+    //         console.log(data);
+    //     return data;
+    // }
+    // pixabay()
+    //     .then(response => console.log(response))
+    //     .catch(err => console.log(err.message))
 
-
-
-
-async function pixabay(){
-    const response = await axios.get(`https://pixabay.com/api/?key=34023502-430bec32b806d37b8bfdc9ad2&q&image_typ=photo&orientation=horizontal&safesearch=true`);
-    console.log(response)
-
-    //     const response = await fetch(`https://pixabay.com/api/?key=34023502-430bec32b806d37b8bfdc9ad2&q&image_typ=photo&orientation=horizontal&safesearch=true`);
-        if (!response.ok) {
-            throw new Error(response.statusText)
-        }
-        const data = await response.json()
-        console.log(data);
-    return data;
+    try {
+        const response = await axios.get(`https://pixabay.com/api/?key=34023502-430bec32b806d37b8bfdc9ad2&q&image_typ=photo&orientation=horizontal&safesearch=true`);
+        gallery.innerHTML =  card(response.data)
+    
+    } catch (err) {
+        console.log(err)
+    }
 }
-pixabay()
-    .then(response => console.log(response))
-    .catch(err => console.log(err))
