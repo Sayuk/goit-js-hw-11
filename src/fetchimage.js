405fac45e2363d37dbd603a6665be5adecc17bb4
Sayuk <sayukbohdan@gmail.com>
2023-03-10 21:46:50 +0200

    d

--- src/fetchimage.js
+++ src/fetchimage.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
 export { fetchImages }
 
-
+axios.defaults.baseURL = 'https://pixabay.com/api/'
+const KEY = '34023502-430bec32b806d37b8bfdc9ad2'
 
 async function fetchImages(query, page, perPage) {
-  const response = await axios.get(`https://pixabay.com/api/?key=34023502-430bec32b806d37b8bfdc9ad2&q&image_typ=photo&orientation=horizontal&safesearch=true`);
+  const response = await axios.get(
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+  )
   return response
 }
