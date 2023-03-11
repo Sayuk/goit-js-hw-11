3cd035d43602c86fd7c1a8be86e42e87aea298a0
Sayuk <sayukbohdan@gmail.com>
2023-03-11 22:28:13 +0200

    s

--- src/index.js
+++ src/index.js
@@ -83,17 +83,17 @@ function onLoadMoreBtn() {
 }
 
 function alertImagesFound(data) {
-  Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+  Notiflix.success(`Hooray! We found ${data.totalHits} images.`)
 }
 
 function alertNoEmptySearch() {
-  Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.')
+  Notiflix.failure('The search string cannot be empty. Please specify your search query.')
 }
 
 function alertNoImagesFound() {
- Notiflix.Notify.success('Sorry, there are no images matching your search query. Please try again.')
+ Notiflix.success('Sorry, there are no images matching your search query. Please try again.')
 }
 
 function alertEndOfSearch() {
-  Notiflix.Notify.success("We're sorry, but you've reached the end of search results.")
+  Notiflix.success("We're sorry, but you've reached the end of search results.")
 }
