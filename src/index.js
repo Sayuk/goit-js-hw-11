72b13431a309272aae85a5634f369461ee89257a
Sayuk <sayukbohdan@gmail.com>
2023-03-12 19:42:45 +0200

    j

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './sass/gallery.scss';
 import './partials/gallery.html';
 import itemsTemplate from './template/index.hbs';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 import NewsApiService from './js/news-servise';
 import LoadMoreButton from './js/load-more-btn';
 
@@ -22,97 +22,79 @@ const refs = {
   loadMoreBtn: document.querySelector('.load-more'),
 };
 
-
-
-
 refs.searchForm.addEventListener('submit', onSearch);
 loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
 
-
-
 async function onSearch(e) {
-  e.preventDefault();        //Чтоб не перезагружалась страничка при субмите формы
-  
+  e.preventDefault(); //Чтоб не перезагружалась страничка при субмите формы
+
   if (!refs.loadMoreBtn.classList.contains('is-hidden')) {
     refs.loadMoreBtn.classList.add('is-hidden');
-    };
-  
+  }
+
   inputText.searchQuery = e.currentTarget.elements.searchQuery.value;
   inputText.resetPage();
 
-   try {
-  if(inputText.searchQuery === '') {
-    clearList();
-    Notiflix.Notify.failure('Please enter your search data.');
+  try {
+    if (inputText.searchQuery === '') {
+      clearList();
+      Notiflix.Notify.failure('Please enter your search data.');
+    } else {
+      loadMoreBtn.show();
+      loadMoreBtn.disable();
+      const response = await inputText.makesRequest();
+      const {
+        data: { hits, total, totalHits },
+      } = response;
+      clearList();
+      loadMoreBtn.enable();
+
+      if (hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        refs.loadMoreBtn.classList.remove('is-hidden');
+        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+        createGalleryList(hits);
+      }
+
+      loadMoreBtn.show();
+    }
+  } catch (error) {
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+    console.log(error.message);
+
+    loadMoreBtn.enable();
   }
-  else {
-    loadMoreBtn.show();
-    loadMoreBtn.disable();
-    const response = await inputText.makesRequest();
-    const {
-      data: { hits, total, totalHits },
-          } = response;
-          clearList();
-          loadMoreBtn.enable();
-  
-          
-
-  if (hits.length === 0) {
-    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-  } else {
-    refs.loadMoreBtn.classList.remove('is-hidden');
-  Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-  createGalleryList(hits);
-  }
-
-  loadMoreBtn.show();
-
-
 }
-
-} catch (error) {
-Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
-console.log(error.message);
-
-loadMoreBtn.enable();
-}
-};
-
-      
 
 async function onLoadMore() {
+  const response = await inputText.makesRequest();
+  const {
+    data: { hits },
+  } = response;
 
-const response = await inputText.makesRequest();
-const {
-  data: { hits },
-} = response;
-
-if (hits.length === 0) {
-  Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-} else createGalleryList(hits); 
-};
-
-
-
-
+  if (hits.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } else createGalleryList(hits);
+}
 
 async function createGalleryList(hits) {
+  const markup = itemsTemplate(hits);
 
-const markup = itemsTemplate(hits);
+  refs.gallery.insertAdjacentHTML('beforeend', markup); //Вставляет результат вызова шаблона
 
-
-
-refs.gallery.insertAdjacentHTML('beforeend', markup);                                                   //Вставляет результат вызова шаблона
-
-simpleLightbox();
-scroll();
-  
-};
-
-
+  simpleLightbox();
+  scroll();
+}
 
 function clearList() {
-  refs.gallery.innerHTML = '';              //Очищает контейнер при сл.запросе поиска
+  refs.gallery.innerHTML = ''; //Очищает контейнер при сл.запросе поиска
 }
 
 function simpleLightbox() {
@@ -122,7 +104,7 @@ function simpleLightbox() {
     enableKeyboard: true,
     doubleTapZoom: 5,
   });
-  lightbox.refresh();
+  // lightbox.refresh();
 }
 
 function scroll() {
--- src/js/news-servise.js
+++ src/js/news-servise.js
@@ -1,48 +1,43 @@
 import axios from 'axios';
 
-
 const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '34023502-430bec32b806d37b8bfdc9ad2';
 
-
 export default class NewsApiService {
-    constructor() {
-      this.searchQuery = '';
-      this.perPage = 40;
-      this.page = 1;
-    };
+  constructor() {
+    this.searchQuery = '';
+    this.perPage = 40;
+    this.page = 1;
+  }
 
-async makesRequest () {
-        const params = new URLSearchParams({
-            key: API_KEY,   //ключ IPI 
-            q: this.searchQuery,
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch: true,
-            page: this.pageAmount,
-            per_page: this.perPage,
-        });
-        const url = `${BASE_URL}/?${params}`;
-        this.incrementPage();
-        return await axios.get(url);
-    }; 
+  async makesRequest() {
+    const params = new URLSearchParams({
+      key: API_KEY, //ключ IPI
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: this.pageAmount,
+      per_page: this.perPage,
+    });
+    const url = `${BASE_URL}/?${params}`;
+    this.incrementPage();
+    return await axios.get(url);
+  }
 
+  incrementPage() {
+    this.page += 1; //Добавление сл.страницы/увеличение
+  }
 
+  resetPage() {
+    this.page = 1; //След.поиск с 1 стр./сброс
+  }
 
-
-    incrementPage() {
-        this.page += 1;           //Добавление сл.страницы/увеличение
-    }
-
-    resetPage() {
-        this.page = 1;            //След.поиск с 1 стр./сброс
-    }
-
-    get query() {
-        return this.searchQuery;
-    }
-                                                //Контролирует термин запроса
-    set query(newQuery) {
-        this.searchQueary = newQuery;
-    }
+  get query() {
+    return this.searchQuery;
+  }
+  //Контролирует термин запроса
+  set query(newQuery) {
+    this.searchQueary = newQuery;
+  }
 }
